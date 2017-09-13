254e72cf6159eb9a81611e9e51778bb3108cfb4c
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-13 16:44:14 +0700

    add sass loader config

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,16 @@ module.exports = {
                 use: [
                     'file-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
             }
         ]
     },
