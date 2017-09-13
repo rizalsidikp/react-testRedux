ea91c3c4b73e798654e6749be4a8104ef6cc4579
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-13 16:31:39 +0700

    add setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                enforce: "pre",
+                enforce: "post",
+                loader: "babel-loader",
+                options: {
+                    presets: ["es2015", "react"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "src")
+        ],
+        extensions: [".js", ".json", ".jsx", ".css"]
+    },
+
+    devtool: "source-map",
+
+    context: __dirname,
+
+    target: "web",
+
+    devServer: {
+       historyApiFallback: true,
+       contentBase: './'
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+
+    node: {
+        console: true,
+        net: 'empty',
+        tls: 'empty',
+        dns: 'empty'
+    }
+}
