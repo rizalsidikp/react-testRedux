7e85de67771f4f2c345ac4255df75d9a3e7f96c1
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-14 13:37:56 +0700

    add new rules

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 enforce: "post",
                 loader: "babel-loader",
                 options: {
-                    presets: ["es2015", "react"]
+                    presets: ["es2015", "react", "stage-1"]
                 }
             },
             {
