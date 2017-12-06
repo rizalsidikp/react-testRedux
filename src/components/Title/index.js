7228c1fc5a01b3952dfaea905f2be89528e80aca
syifanurul <syifanurul99@gmail.com>
2017-12-06 10:42:04 +0700

    change-background

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styles from './style.scss';
 
 class Title extends Component {
     render(){
         return(
-            <h1>{ this.props.title }</h1>
+            <h1 className='Title'>{ this.props.title }</h1>
         )
     }
 }
